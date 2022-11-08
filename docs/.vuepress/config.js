2b0dc577065ea83ecbf61b72c419ce633efa223e
heerting <710945475@qq.com>
2022-11-08 11:40:38 +0800

    blog init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: '博客文档',
+    description: 'sister的博客文档',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: 'heyiting的博客',
+                items: [
+                    { text: 'Github', link: 'https://github.com' },
+                    { text: '掘金', link: 'https://juejin.cn' }
+                ]
+            }
+        ]
+    }
+}
