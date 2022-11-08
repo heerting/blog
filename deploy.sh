e3448dded01c90aea916e86e2ad80180a47b3657
heerting <710945475@qq.com>
2022-11-08 16:33:41 +0800

    build

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+yarn docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -12,8 +12,10 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
+git branch -M main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:heerting/blog.git master:main
+# 如果你想要部署到 https://USERNAME.github.io
+git push -f git@github.com:heerting/heerting.github.io.git main
 
 cd -
